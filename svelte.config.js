f2308d2b1f6d3907415ceb7cad391c9ab5f52793
Sensanaty <salevic@lu-ka.me>
2021-10-08 17:20:38 +0200

    Initialize repo with some Svelte options setup

---
+++ svelte.config.js
@@ -0,0 +1,31 @@
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static"
+import path from "path";
+
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
+
+  kit: {
+    target: "#svelte",
+    adapter: adapter(),
+    files: {
+      assets: "public",
+      routes: "src/pages"
+    },
+    prerender: {
+      enabled: true
+    },
+    trailingSlash: "never",
+    vite: {
+      resolve: {
+        alias: {
+          "@": path.resolve("./src")
+        }
+      }
+    }
+  },
+};
+
+export default config;
