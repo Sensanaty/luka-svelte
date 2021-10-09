995f9895ec735b4dd5eea38156c1552345be013f
Sensanaty <salevic@lu-ka.me>
2021-10-09 13:14:24 +0200

    Add Roboto to the project & load it in global __layout

---
+++ src/pages/__layout.svelte
@@ -0,0 +1,19 @@
+<slot />
+
+<style lang="scss">
+  @mixin fontFamily($font-name, $font-path, $weight: normal, $style: normal) {
+    @font-face {
+      font-family: $font-name;
+      src: url($font-path + '.eot');
+      src: url($font-path + '.woff2') format('woff2'),
+      url($font-path + '.woff') format('woff'),
+      url($font-path + '.ttf') format('truetype');
+      font-weight: $weight;
+      font-style: $style;
+      font-display: swap;
+    }
+  }
+
+  // Roboto
+  @include fontFamily("Roboto", "../assets/fonts/Roboto/Roboto-Bold", bold)
+</style>
