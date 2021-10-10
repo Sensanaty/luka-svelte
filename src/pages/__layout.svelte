35435035871c7145a2f2e1ac451a306db77f2884
Sensanaty <salevic@lu-ka.me>
2021-10-10 17:10:15 +0200

    Optimize font delivery to avoid FOUT
    - Removed .ttf and .eot font formats
    - Removed the SCSS @font-face declarations and moved them to the global stylesheet
    - Removed the app.html `font-prefetch` span

--- src/pages/__layout.svelte
+++ src/pages/__layout.svelte
@@ -14,25 +14,3 @@
     },
   })
 </script>
-
-<style lang="scss">
-  @mixin fontFamily($font-name, $font-path, $weight: normal, $style: normal) {
-    @font-face {
-      font-family: $font-name;
-      src: url($font-path + '.eot');
-      src: url($font-path + '.woff2') format('woff2'),
-      url($font-path + '.woff') format('woff'),
-      url($font-path + '.ttf') format('truetype');
-      font-weight: $weight;
-      font-style: $style;
-      font-display: swap;
-    }
-  }
-
-  // Roboto
-  @include fontFamily("Roboto", "/fonts/Roboto/Roboto-Bold", bold);
-
-  // Aeonik
-  @include fontFamily("Aeonik", "/fonts/Aeonik/Aeonik-Bold", bold);
-  @include fontFamily("Aeonik", "/fonts/Aeonik/Aeonik-Medium", 500);
-</style>
