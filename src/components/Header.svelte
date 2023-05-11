d4255be413dd27b88c747ca781d76e2ede8eba74
Luka <salevic@lu-ka.me>
2023-05-12 00:03:47 +0200

    Fix improper tailwind padding in Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,9 @@
 <script>
-import ThemeToggler from "./ThemeToggler.svelte";
+import ThemeToggler from "@/components/ThemeToggler.svelte";
 </script>
 
 <template>
-  <header class="grid flex-wrap items-center bg-bg-highlight p-2 px-5 text-lg font-bold w-full">
+  <header class="grid flex-wrap items-center bg-bg-highlight py-2 px-5 text-lg font-bold w-full">
     <h1>
       luka salević<span aria-hidden="true" class="select-none">...</span><span aria-hidden="true" class="ibeam select-none">|</span>
     </h1>
