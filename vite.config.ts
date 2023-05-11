020407d6d4dba321b14646e9ed57b40a37d5f0da
Luka <salevic@lu-ka.me>
2023-05-11 21:54:35 +0200

    Add node typings & path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
+import path from "path";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
+const resolvedPath = path.resolve(__dirname, "src");
+
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      "@/": `${resolvedPath}/`
+    }
+  }
 });
