18a482d778cd1b536c681fee7c3189cb910c67a3
Luka <salevic@lu-ka.me>
2023-05-14 12:23:48 +0200

    Create 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center mx-auto">
+  <h1 class="text-9xl text-accent font-black mb-3">404</h1>
+  <h2 class="text-4xl font-bold">sorry, this page doesn't exist. Want to go <a href="/">back</a> instead?</h2>
+</div>
