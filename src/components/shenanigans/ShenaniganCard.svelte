0a97d8d5398b16b2c7b35055e76c76fad3c3f7a5
Luka <salevic@lu-ka.me>
2023-05-16 17:11:28 +0200

    Finalize the shenanigans page and introduce a new ShenaniganCard component

---
+++ src/components/shenanigans/ShenaniganCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let title: string;
+  export let subtitle: string;
+  export let route: string;
+</script>
+
+<a class="{$$restProps.class} flex flex-col items-start p-3 cursor-pointer bg-highlight hover:bg-zinc-800 no-underline text-cl" href="/shenanigans/{route}">
+  <div class="mb-2 border-b-2 border-accent border-dotted w-full">
+    <h1 class="text-2xl pb-1 font-black">{title}</h1>
+  </div>
+  <h2 class="font-bold">{subtitle}</h2>
+</a>
---
+++ src/routes/shenanigans/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import ShenaniganCard from "@/components/shenanigans/ShenaniganCard.svelte";
+  import shenaniganList from "@/data/shenanigans";
+
+  let shenanigans = shenaniganList;
+</script>
+
+<div>
+  <h1 class="mb-5">
+    This page serves as a sort of central hub for any random things I want to play around with, be it random WebGL
+    experiments, playing around with 3D/canvas stuff or trying out random node packages. Basically just a random
+    collection of fun stuff done to expand my knowledge in one way or another
+  </h1>
+
+  <div class="grid grid-cols-3 gap-2 gap-y-5 w-full">
+    {#each shenanigans as shenanigan}
+      <ShenaniganCard title={shenanigan.title} subtitle={shenanigan.subtitle} route={shenanigan.route} />
+    {/each}
+  </div>
+</div>
+
