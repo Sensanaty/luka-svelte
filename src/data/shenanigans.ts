a12d3b65996ab2f424c9afdf4b5bbaf0c6951bdf
Luka <salevic@lu-ka.me>
2023-07-06 10:04:09 +0200

    Create the new three.js page with basic text examples

--- src/data/shenanigans.ts
+++ src/data/shenanigans.ts
@@ -3,7 +3,12 @@ const list = [
     title: "Svelte-Powered window dragging",
     subtitle: "svelte makes it impressively easy to have smooth window dragging",
     route: "svelte-dragging"
-  }
+  },
+  {
+    title: "Experimenting with three.js",
+    subtitle: "playing around with three.js and seeing what I can accomplish",
+    route: "threejs",
+  },
 ];
 
 export default list;
---
+++ src/routes/shenanigans/(list)/threejs/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Infobox from "@/components/Infobox.svelte";
+</script>
+
+<div class="flex flex-col">
+  <div class="title-box lg:w-max">
+    <h1>EZ 3D</h1>
+  </div>
+
+  <p class="leading-7 mb-4">
+    <a href="https://threejs.org" target="_blank" rel="noopener">three.js</a> is a really cool library that lets you create interactive 3D scenes in the browser. I've always had an interest in 3D-related things, so I thought why not give it a try to do it myself?
+  </p>
+
+  <Infobox
+    text="All examples below are loaded asynchronously to save bandwith, so just click on the button to load the relevant example"
+  />
+</div>
+
+<style>
+  .title-box {
+    @apply mb-4 pb-1 border-b-2 border-accent border-dotted text-3xl font-black;
+  }
+</style>
