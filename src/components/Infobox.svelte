aa211a00b6c0210515836deb6adaba6a1c768b2e
Luka <salevic@lu-ka.me>
2023-07-06 09:57:38 +0200

    Create a Infobox component

---
+++ src/components/Infobox.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let text: string;
+  export let type: "info" | "warning" = "info";
+</script>
+
+<div class="bg-highlight px-3 py-2 mb-5 flex items-center w-fit">
+  <!--  eslint-disable quotes  -->
+  <span
+    class="rounded-full border-2 px-2 mr-4 select-none {type === 'info' ? 'border-accent' : 'border-rose-600'}"
+    aria-hidden="true"
+  >
+  <!--  eslint-enable quotes  -->
+    {#if type === "info"}
+      i
+    {:else}
+      !
+    {/if}
+  </span>
+
+  <p>{text}</p>
+</div>
