c2907095d7de0874a3c53db4fd93f08effa4d2a0
Luka <salevic@lu-ka.me>
2023-07-06 09:57:18 +0200

    Add alternative hover color based on theme in ShenaniganCards

--- src/components/shenanigans/ShenaniganCard.svelte
+++ src/components/shenanigans/ShenaniganCard.svelte
@@ -4,7 +4,7 @@
   export let route: string;
 </script>
 
-<a class="{$$restProps.class} flex flex-col items-start p-3 cursor-pointer bg-highlight hover:bg-zinc-800 no-underline text-cl" href="/shenanigans/{route}">
+<a class="{$$restProps.class} flex flex-col items-start p-3 cursor-pointer bg-highlight hover:bg-zinc-800 dark:hover:bg-amber-50 no-underline text-cl" href="/shenanigans/{route}">
   <div class="mb-2 border-b-2 border-accent border-dotted w-full">
     <h1 class="text-2xl pb-1 font-black">{title}</h1>
   </div>
